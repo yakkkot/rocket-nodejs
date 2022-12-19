a5dbb1ee8e662f7a229ed8534a25bcf4ad8d7db0
yakkkot <k0tjeee777@gmail.com>
2022-12-19 18:29:29 +0200

    dz_2

--- api/users/users.services.js
+++ api/users/users.services.js
@@ -1,14 +1,17 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
+
+const pathDB = path.join('dataBase', 'data.json');
+
 module.exports = {
     readData: async () => {
-        const users = await fs.readFile(path.join('dataBase', 'data.json'));
+        const users = await fs.readFile(pathDB);
         return JSON.parse(users.toString())
     },
 
     writeData: async (users) => {
-        await fs.writeFile(path.join('dataBase', 'data.json'), JSON.stringify(users))
+        await fs.writeFile(pathDB, JSON.stringify(users))
     },
 
     checkName: async (usersDB, userBody) => {
